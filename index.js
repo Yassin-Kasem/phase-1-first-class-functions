94dd317ed55eed8e67a4b93211cba9eb553a53aa
Yassin-Kasem <145336220+Yassin-Kasem@users.noreply.github.com>
2024-02-27 15:20:22 +0300

    task compleate

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("named function");
+    };
+}
+
+
+function returnsAnAnonymousFunction() {
+    return () => {
+        console.log("anonymous function");
+    };
+}
+
+
